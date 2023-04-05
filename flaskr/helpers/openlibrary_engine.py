81a7a5776bfe31e9d7d9b0eb82f997024296e7b7
Tyler Starling <tstarling25.ts@gmail.com>
2023-04-05 13:03:48 -0400

    - Added boostrap for html styling -TODO: Add bootstrap to login -TODO: add bootstrap to register

--- flaskr/helpers/openlibrary_engine.py
+++ flaskr/helpers/openlibrary_engine.py
@@ -1,11 +1,14 @@
 from olclient.openlibrary import OpenLibrary
 import olclient.common as common
 
+from ..data_store.db import get_db
+
 import pprint
 
 class OpenLibraryClient():
     def __init__(self):
         self.client = OpenLibrary()
+        self.db = get_db()
 
     def search_isbn(self, isbn):
         return_value = None
@@ -20,6 +23,7 @@ class OpenLibraryClient():
         author = self.client.Author.get(olid=olid)        
         return author
     
+    
     def get_author_from_work(self, work):
         author = work.authors[0]
         if author is not None:          
@@ -28,6 +32,9 @@ class OpenLibraryClient():
             author = self.get_author(author_olid)
 
         return author
+
+    def _check_db(self, first_name, last_name):
+        return self.db.execute("SELECT * FROM authors WHERE first_name = ? AND last_name = ?", (first_name, last_name))
 
 # pp = pprint.PrettyPrinter()  
 # ol = OpenLibraryClient()
