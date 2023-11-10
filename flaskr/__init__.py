7c76f36192e0c6e3f333784b2dda2c0aa522b460
UbiquitousKiwi <tstarling25.ts@gmail.com>
2023-11-09 21:05:20 -0500

    Removed scanner page due to shared library issue on app platform

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,17 +2,17 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # Load the test config if passed in
         app.config.from_mapping(test_config)
@@ -22,19 +22,22 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
     from .data_store import db
+
     db.init_app(app)
 
     from .pages import auth
+
     app.register_blueprint(auth.bp)
 
     from .pages import home
-    app.register_blueprint(home.bp)
-    app.add_url_rule('/', endpoint='index')
 
-    from .pages import scanner
-    app.register_blueprint(scanner.bp)
+    app.register_blueprint(home.bp)
+    app.add_url_rule("/", endpoint="index")
+
+    # from .pages import scanner
+    # app.register_blueprint(scanner.bp)
 
     app.app_context().push()
 
