0c40ec66e4e0cc60f6d9064ea811949bef50fff2
UbiquitousKiwi <tstarling25.ts@gmail.com>
2023-12-04 21:13:16 -0500

    Switched db scheme to email based system

--- flaskr/models/book.py
+++ flaskr/models/book.py
@@ -4,8 +4,17 @@ from flaskr.data_store.db import get_db
 
 from datetime import timedelta, datetime
 
+
 class Book(BaseObject):
-    def __init__(self, book_id=None, title=None, isbn=None, author=None, cover_url=None, checkout_log=None):
+    def __init__(
+        self,
+        book_id=None,
+        title=None,
+        isbn=None,
+        author=None,
+        cover_url=None,
+        checkout_log=None,
+    ):
         self.id = book_id
         self.title = title
         self.isbn = isbn
@@ -49,14 +58,15 @@ class Book(BaseObject):
                     title = ?, author_id = ?, isbn = ?, illustration_url = ?
                 """
         query_params = [
-            self.title, 
-            self.author_id, 
-            self.isbn, 
-            self.cover_url, 
-            self.title, 
-            self.author_id, 
-            self.isbn, 
-        self.cover_url]
+            self.title,
+            self.author_id,
+            self.isbn,
+            self.cover_url,
+            self.title,
+            self.author_id,
+            self.isbn,
+            self.cover_url,
+        ]
         self.db.execute(query, query_params)
         self.db.commit()
 
@@ -71,7 +81,7 @@ class Book(BaseObject):
         query = self._inflate_query_base.format(where_clause)
         query_params = [isbn]
         self._inflate(query, query_params)
-    
+
     def inflate_by_title(self, title):
         where_clause = "book.title = ?"
         query = self._inflate_query_base.format(where_clause)
@@ -82,15 +92,15 @@ class Book(BaseObject):
         self._check_db()
         retrieved_book = self._db.execute(query, query_params).fetchone()
 
-        self.id = retrieved_book['id']
-        self.title = retrieved_book['title']
-        self.isbn = retrieved_book['isbn']
-        self.cover_url = retrieved_book['illustration_url']
+        self.id = retrieved_book["id"]
+        self.title = retrieved_book["title"]
+        self.isbn = retrieved_book["isbn"]
+        self.cover_url = retrieved_book["illustration_url"]
 
         author = Author()
-        author.first_name = retrieved_book['author_first_name']
-        author.last_name = retrieved_book['author_last_name']
-        author.olid = retrieved_book['author_olid']
+        author.first_name = retrieved_book["author_first_name"]
+        author.last_name = retrieved_book["author_last_name"]
+        author.olid = retrieved_book["author_olid"]
 
         self.author = author
         self._inflate_checkout()
@@ -102,7 +112,6 @@ class Book(BaseObject):
             SELECT
                 u.first_name,
                 u.last_name,
-                u.library_card_number,
                 cl.checkout_date,
                 cl.checkin_date,
                 cl.checkout_duration,
@@ -110,9 +119,9 @@ class Book(BaseObject):
             FROM
                 checkout_log cl
             LEFT JOIN
-                user u
+                patron u
             ON
-                cl.user_id = u.id 
+                cl.patron_id = u.id 
             WHERE
                 cl.deleted = false
                 AND cl.book_id = ?
@@ -120,17 +129,20 @@ class Book(BaseObject):
         query_params = [self.id]
         results = self._db.execute(query, query_params)
         for row in results:
-            calc_return = timedelta(days=row['checkout_duration'])
-            calc_return = row['checkout_date'] + calc_return
-            returned = row['checkin_date'] is None
-            checkout_log.append({
-                'first_name': row['first_name'], 
-                'last_name': row['last_name'], 
-                'returned': returned, 
-                'checkout_date': row['checkout_date'],
-                'checkin_date': row['checkin_date'], 
-                'renew_count': row['renew_count'], 
-                'expected_return': calc_return})
+            calc_return = timedelta(days=row["checkout_duration"])
+            calc_return = row["checkout_date"] + calc_return
+            returned = row["checkin_date"] is None
+            checkout_log.append(
+                {
+                    "first_name": row["first_name"],
+                    "last_name": row["last_name"],
+                    "returned": returned,
+                    "checkout_date": row["checkout_date"],
+                    "checkin_date": row["checkin_date"],
+                    "renew_count": row["renew_count"],
+                    "expected_return": calc_return,
+                }
+            )
         self.checkout_log = checkout_log
 
     def _check_db(self):
@@ -139,15 +151,21 @@ class Book(BaseObject):
 
     def _error_check(self):
         if type(self.id) is not type(int):
-            raise TypeError(f"Book ID can not be {type(self.id)}, must be int. Check book table in DB.")
+            raise TypeError(
+                f"Book ID can not be {type(self.id)}, must be int. Check book table in DB."
+            )
         if type(self.title) is not type(str):
             raise TypeError(f"Book title can not be {type(self.title)}, must be str")
         if type(self.isbn) is not type(int):
             raise TypeError(f"Book ISBN can not be {type(self.isbn)}, must be int.")
         if type(self.author_id) is not type(int):
-            raise TypeError(f"Book author_id can not be {type(self.author_id)}, must be int")
+            raise TypeError(
+                f"Book author_id can not be {type(self.author_id)}, must be int"
+            )
         if type(self.cover_url) is not type(str):
-            raise TypeError(f"Book cover_url can not be {type(self.cover_url)}, must be str")
-        
+            raise TypeError(
+                f"Book cover_url can not be {type(self.cover_url)}, must be str"
+            )
+
     def __str__(self):
         return f"{self.title}"
