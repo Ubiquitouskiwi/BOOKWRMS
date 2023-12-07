685a6863a45ecf1e824aa97679091cfaac24b731
Tyler Starling <tylstarl@amazon.com>
2023-12-07 10:41:41 -0500

    Fixed session issue

--- flaskr/pages/home.py
+++ flaskr/pages/home.py
@@ -21,18 +21,6 @@ from ..helpers.openlibrary_engine import OpenLibraryClient
 bp = Blueprint("home", __name__)
 
 
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session["user_id"]
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = (
-            get_db().execute("SELECT * FROM user WHERE id = ?", [user_id]).fetchone()
-        )
-
-
 @bp.route("/")
 def index():
     db = get_db()
