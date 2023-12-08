aa1101af81a5b8924ac4496c939b32ee34763788
Tyler Starling <tylstarl@amazon.com>
2023-12-08 09:04:56 -0500

    Fixed facing mode spelling error

--- flaskr/static/js/scanner.js
+++ flaskr/static/js/scanner.js
@@ -8,16 +8,16 @@ let streamStarted = false;
 const constraints = {
     video: {
         width: {
-            min: 480,
+            min: 1280,
             ideal: 1920,
             max: 2560
         },
         height: {
-            min: 480,
+            min: 720,
             ideal: 1080,
             max: 1440
         },
-        faceingMode: 'environment'
+        facingMode: 'environment'
     }
 }
 
@@ -68,7 +68,6 @@ Quagga.init({
         readers: ["ean_reader"]
     },
     multiple: false,
-    halfSample: false,
 }, function (err) {
     if (err) {
         console.log(err);
